91c00ab494628efbde875b4d3a0687cfd6f0c730
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-03-28 01:27:08 +0800

    first commit

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,15 @@
+local options = {
+  ensure_installed = { "lua", "vim", "vimdoc" },
+
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+  endwise = { enable = true },
+}
+
+return options
